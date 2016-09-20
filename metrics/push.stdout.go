b1967f5139b8105134401f6df51439b304e43403
Tomasz Adamski <tadamski@wikia-inc.com>
2016-09-20 15:58:55 +0200

    escaping tests

--- metrics/push.stdout.go
+++ metrics/push.stdout.go
@@ -18,16 +18,39 @@ func OutputMetrics(filteredMetrics []models.FilteredMetrics, writer io.Writer) e
 
 		tagKeysAndValues := []string{}
 		for tagKey, tagValue := range metric.Tags {
-			tagKeysAndValues = append(tagKeysAndValues, fmt.Sprintf("%s=%s", tagKey, tagValue))
+			tagKeysAndValues = append(tagKeysAndValues, fmt.Sprintf("%s=%s", escapeCommasEqualSignsAndSpaces(tagKey), escapeCommasEqualSignsAndSpaces(tagValue)))
 		}
 		tags := strings.Join(tagKeysAndValues, ",")
 
 		fieldKeysAndValues := []string{}
 		for fieldKey, fieldValue := range metric.Fields {
-			fieldKeysAndValues = append(fieldKeysAndValues, fmt.Sprintf("%s=%s", fieldKey, fieldValue))
+			valueFormat := "%s"
+			switch fieldValue.(type) {
+			case string:
+				valueFormat = "%q" //puts into quotes and escape quotes
+			case uint, uint8, uint16, uint32, uint64, int, int8, int16, int32, int64:
+				valueFormat = "%d"
+			case float32, float64:
+				valueFormat = "%g"
+			case bool:
+				valueFormat = "%t"
+			}
+
+			fieldKeysAndValues = append(fieldKeysAndValues, fmt.Sprintf(fmt.Sprintf("%%s=%s", valueFormat), escapeCommasEqualSignsAndSpaces(fieldKey), fieldValue))
 		}
 		fields := strings.Join(fieldKeysAndValues, ",")
-		fmt.Fprint(writer, fmt.Sprintf("%s,%s %s\n", "resources", tags, fields))
+		if (len(tags) == 0) {
+			fmt.Fprint(writer, fmt.Sprintf("%s %s\n", "resources", fields))
+		} else {
+			fmt.Fprint(writer, fmt.Sprintf("%s,%s %s\n", "resources", tags, fields))
+		}
 	}
 	return nil
+}
+
+func escapeCommasEqualSignsAndSpaces(value string) string {
+	value = strings.Replace(value, " ", "\\ ", -1)
+	value = strings.Replace(value, ",", "\\,", -1)
+	value = strings.Replace(value, "=", "\\=", -1)
+	return value
 }
--- metrics/push.stdout_test.go
+++ metrics/push.stdout_test.go
@@ -19,7 +19,7 @@ func TestOutputMetricsWhenFiltersEmpty(t *testing.T) {
 func TestShouldWriteToOutputWhenNotEmptyFilters(t *testing.T) {
 	//given
 	testObj := new(MockedWriter)
-	expectedOutputLine := []byte("resources,exampleTag=exampleTagValue exampleField=exampleFieldValue\n")
+	expectedOutputLine := []byte("resources,exampleTag=exampleTagValue exampleField=\"exampleFieldValue\"\n")
 	testObj.On("Write", expectedOutputLine).Return(len(expectedOutputLine), nil)
 	metric := models.NewFilteredMetric()
 	metric.Tags["exampleTag"] = "exampleTagValue"
@@ -41,6 +41,44 @@ func TestShouldErrorWhenNoFields(t *testing.T) {
 	assert.NotNil(t, err)
 }
 
+func TestShouldHandleNoTags(t *testing.T) {
+	//given
+	testObj := new(MockedWriter)
+	expectedOutputLine := []byte("resources exampleField=\"exampleFieldValue\"\n")
+	testObj.On("Write", expectedOutputLine).Return(len(expectedOutputLine), nil)
+	metric := models.NewFilteredMetric()
+	metric.Fields["exampleField"] = "exampleFieldValue"
+
+	//when
+	OutputMetrics([]models.FilteredMetrics{metric}, testObj)
+
+	//then
+	testObj.AssertExpectations(t)
+}
+
+func TestShouldHandleDifferentFieldTypes(t *testing.T) {
+	//given
+	testObj := new(MockedWriter)
+	expectedOutputLine := []byte("resources stringField=\"exampleFieldValue\",intField=1,floatField=1.21,booleanField=true\n")
+	testObj.On("Write", mock.Anything).Return(len(expectedOutputLine), nil)
+	metric := models.NewFilteredMetric()
+	metric.Fields["stringField"] = "example Field\\Value"
+	metric.Fields["intField"] = 1
+	metric.Fields["floatField"] = 1.21
+	metric.Fields["booleanField"] = true
+
+	//when
+	OutputMetrics([]models.FilteredMetrics{metric}, testObj)
+
+	//then
+	testObj.AssertExpectations(t)
+	calledWithMessage := string(testObj.Calls[0].Arguments.Get(0).([]byte))
+	assert.Contains(t, calledWithMessage, "stringField=\"example Field\\\\Value\"")
+	assert.Contains(t, calledWithMessage, "intField=1")
+	assert.Contains(t, calledWithMessage, "floatField=1.21")
+	assert.Contains(t, calledWithMessage, "booleanField=true")
+}
+
 func TestShouldOutputMultipleMetrics(t *testing.T) {
 	//given
 	testObj := new(MockedWriter)
@@ -52,12 +90,54 @@ func TestShouldOutputMultipleMetrics(t *testing.T) {
 	metric2.Tags["exampleTag2"] = "exampleTagValue2"
 	metric2.Fields["exampleField2"] = "exampleFieldValue2"
 
-	expectedOutputLine1 := []byte("resources,exampleTag1=exampleTagValue1 exampleField1=exampleFieldValue1\n")
-	expectedOutputLine2 := []byte("resources,exampleTag2=exampleTagValue2 exampleField2=exampleFieldValue2\n")
+	expectedOutputLine1 := []byte("resources,exampleTag1=exampleTagValue1 exampleField1=\"exampleFieldValue1\"\n")
+	expectedOutputLine2 := []byte("resources,exampleTag2=exampleTagValue2 exampleField2=\"exampleFieldValue2\"\n")
 	testObj.On("Write", expectedOutputLine1).Return(len(expectedOutputLine1), nil).On("Write", expectedOutputLine2).Return(len(expectedOutputLine2), nil)
 	//when
 	OutputMetrics([]models.FilteredMetrics{metric1, metric2}, testObj)
 
+	//then
+	testObj.AssertExpectations(t)
+}
+
+func TestShouldEscapeTagKeys(t *testing.T) {
+	//given
+	testObj := new(MockedWriter)
+	expectedOutputLine := []byte("resources,example\\,T\\=a\\ g=exampleTagValue exampleField=\"exampleFieldValue\"\n")
+	testObj.On("Write", expectedOutputLine).Return(len(expectedOutputLine), nil)
+	metric := models.NewFilteredMetric()
+	metric.Tags["example,T=a g"] = "exampleTagValue"
+	metric.Fields["exampleField"] = "exampleFieldValue"
+	//when
+	OutputMetrics([]models.FilteredMetrics{metric}, testObj)
+	//then
+	testObj.AssertExpectations(t)
+}
+
+func TestShouldEscapeTagValues(t *testing.T) {
+	//given
+	testObj := new(MockedWriter)
+	expectedOutputLine := []byte("resources,exampleTag=example\\,T\\=a\\ gValue exampleField=\"exampleFieldValue\"\n")
+	testObj.On("Write", expectedOutputLine).Return(len(expectedOutputLine), nil)
+	metric := models.NewFilteredMetric()
+	metric.Tags["exampleTag"] = "example,T=a gValue"
+	metric.Fields["exampleField"] = "exampleFieldValue"
+	//when
+	OutputMetrics([]models.FilteredMetrics{metric}, testObj)
+	//then
+	testObj.AssertExpectations(t)
+}
+
+func TestShouldEscapeFieldKeys(t *testing.T) {
+	//given
+	testObj := new(MockedWriter)
+	expectedOutputLine := []byte("resources,exampleTag=exampleTagValue example\\,F\\=i\\ eld=\"exampleFieldValue\"\n")
+	testObj.On("Write", expectedOutputLine).Return(len(expectedOutputLine), nil)
+	metric := models.NewFilteredMetric()
+	metric.Tags["exampleTag"] = "exampleTagValue"
+	metric.Fields["example,F=i eld"] = "exampleFieldValue"
+	//when
+	OutputMetrics([]models.FilteredMetrics{metric}, testObj)
 	//then
 	testObj.AssertExpectations(t)
 }
